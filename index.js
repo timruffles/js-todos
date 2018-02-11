6eb47e105c02caa35378dc1fd617130599574650
oprogramador <uomodislesia@gmail.com>
2018-02-12 00:17:01 +0100

    lint

--- index.js
+++ index.js
@@ -1,138 +1,170 @@
-var espree = require("espree")
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-console */
+/* eslint-disable sort-keys */
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
+
+const espree = require('espree');
+const fs = require('fs');
+require('colors'); // extends string proto
+
+const todoRe = commentSafeRe('todo');
+const fixmeRe = commentSafeRe('fixme');
 
 // public API
 function findTodos(src) {
   src = stripShebang(src);
-  var ast = espree.parse(src,{loc: true, comment:true, ecmaVersion: 9});
-  return cmts.commentsToTodos(ast.comments);
+  const ast = espree.parse(src, { loc: true, comment: true, ecmaVersion: 9 });
+
+  return findTodos.commentsToTodos(ast.comments);
 }
 
-var cmts = module.exports = findTodos;
+findTodos.findTodo = (comment) => {
+  let found;
+  findTodos.types.some((fn) => {
+    const issue = fn(comment);
+    if (issue) {
+      found = issue;
 
-cmts.findTodo = function(comment) {
-  var found
-  cmts.types.some(function(fn) {
-    var issue
-    if(issue = fn(comment)) {
-      return found = issue
+      return found;
     }
-  })
-  return found
-}
+  });
 
-cmts.commentsToTodos = function(comments) {
-  var all = []
-  comments.forEach(function(comment) {
-    var issue = cmts.findTodo(comment)
-    if(issue) all.push(issue)
-  })
-  return all
+  return found;
 };
 
-cmts.types = [
-  function(comment) {
-    var val = comment.value
-    var found
-    [{re:todoRe, type:"todo"},{re:fixmeRe, type:"fixme"}].some(function(setup) {
-      var match
-      if(!(match = setup.re.exec(comment.value))) return
-      comment.type = setup.type
-      var linesBefore = comment.value.slice(0,match.index).split(/\n/g)
-      if(linesBefore.length > 0) {
-        comment.loc.start.line += linesBefore.length - 1
-        comment.loc.start.character = linesBefore[linesBefore.length - 1].length
-        comment.value = comment.value.slice(match.index)
+findTodos.commentsToTodos = (comments) => {
+  const all = [];
+  comments.forEach((comment) => {
+    const issue = findTodos.findTodo(comment);
+    if (issue) {
+      all.push(issue);
+    }
+  });
+
+  return all;
+};
+
+findTodos.types = [
+  (comment) => {
+    let found;
+    [{ re: todoRe, type: 'todo' }, { re: fixmeRe, type: 'fixme' }].some((setup) => {
+      const match = setup.re.exec(comment.value);
+      if (!(match)) {
+        return;
       }
-      return found = comment
-    })
-    return found
-  }
-]
-var todoRe = commentSafeRe("todo")
-var fixmeRe = commentSafeRe("fixme")
+      comment.type = setup.type;
+      const linesBefore = comment.value.slice(0, match.index).split(/\n/g);
+      if (linesBefore.length > 0) {
+        comment.loc.start.line += linesBefore.length - 1;
+        comment.loc.start.character = linesBefore[linesBefore.length - 1].length;
+        comment.value = comment.value.slice(match.index);
+      }
+
+      found = comment;
+
+      return found;
+    });
+
+    return found;
+  },
+];
 
 // output
-cmts.readAll = function(paths,output) {
-  var fs = require("fs")
-  paths.forEach(function(path) {
-    output(null,path)
+findTodos.readAll = (paths, output) => {
+  paths.forEach((path) => {
+    output(null, path);
     try {
-      var issues = cmts(fs.readFileSync(path,"utf-8"))
-      issues.forEach(function(issue) {
-        output(issue,path)
-      })
-    } catch(e) {
-      console.error("Error processing " + path + ":" + e);
-      process.exit(1)
+      const issues = findTodos(fs.readFileSync(path, 'utf-8'));
+      issues.forEach((issue) => {
+        output(issue, path);
+      });
+    } catch (e) {
+      console.error(`Error processing ${path}:${e}`);
+      process.exit(1);
     }
-  })
-  output("complete")
-}
+  });
+  output('complete');
+};
 
-cmts.outputters = {
-  default: function(opts) {
-    require("colors") // extends string proto
-    var paths = {}
-    var total = 0
-    var byType = {}
+findTodos.outputters = {
+  default(opts) {
+    const paths = {};
+    let total = 0;
+    const byType = {};
     function finish() {
-      var pathCount = Object.keys(paths).length
-      console.log("")
-      if(total === 0) {
-        return console.log("PASSED".green + " scanned " + pathCount + " " + pluralize("file",pathCount) + ", no todos found")
+      const pathCount = Object.keys(paths).length;
+      console.log('');
+      if (total === 0) {
+        return console.log(`${'PASSED'.green} scanned ${pathCount} ${pluralize('file', pathCount)}, no todos found`);
       }
-      var filesWithTodos = Object.keys(paths).filter(function(path) {
-        return paths[path].length > 0
-      }).length
-      var typeString = Object.keys(byType).map(function(type) {
-        var count = byType[type].length
-        return count + " " + type
-      }).join(", ")
-      console.log("FAILED".red + " found " + total + " todos in " + filesWithTodos + " " + pluralize("file",filesWithTodos) + ": " + typeString)
-      console.log("")
-      Object.keys(paths).forEach(function(path) {
-        var issues = paths[path]
-        if(issues.length === 0) return;
-        console.log(path + ":")
-        issues.forEach(function(issue) {
-          console.log(":" + issue.loc.start.line + " " + issue.value)
-        })
-        console.log("")
-      })
-      if(!opts.nostatus) process.exit(total === 0 ? 0 : 1)
-    }
-    return function(issue,path) {
-      if(issue === "complete") return finish()
-      paths[path] = paths[path] || []
-      if(!issue) return;
-      total += 1
-      paths[path].push(issue)
-      byType[issue.type] = byType[issue.type] || []
-      byType[issue.type].push(issue)
-    }
-  },
-  json: function(opts) {
-    var all = []
-    return function(issue,path) {
-      if(!issue) return;
-      if(issue === "complete") {
-        console.log(JSON.stringify(all))
-        if(!opts.nostatus) process.exit(all.length === 0 ? 0 : 1)
-      }
-      issue.path = path
-      all.push(issue)
-    }
-  }
-}
+      const filesWithTodos = Object.keys(paths).filter(path => paths[path].length > 0).length;
+      const typeString = Object.keys(byType).map((type) => {
+        const count = byType[type].length;
 
-function pluralize(str,n) {
-  return n === 1 ? str : str + "s"
+        return `${count} ${type}`;
+      }).join(', ');
+      // eslint-disable-next-line max-len
+      console.log(`${'FAILED'.red} found ${total} todos in ${filesWithTodos} ${pluralize('file', filesWithTodos)}: ${typeString}`);
+      console.log('');
+      Object.keys(paths).forEach((path) => {
+        const issues = paths[path];
+        if (issues.length === 0) {
+          return;
+        }
+        console.log(`${path}:`);
+        issues.forEach((issue) => {
+          console.log(`:${issue.loc.start.line} ${issue.value}`);
+        });
+        console.log('');
+      });
+      if (!opts.nostatus) {
+        process.exit(total === 0 ? 0 : 1);
+      }
+    }
+
+    return (issue, path) => {
+      if (issue === 'complete') {
+        return finish();
+      }
+      paths[path] = paths[path] || [];
+      if (!issue) {
+        return;
+      }
+      total += 1;
+      paths[path].push(issue);
+      byType[issue.type] = byType[issue.type] || [];
+      byType[issue.type].push(issue);
+    };
+  },
+  json(opts) {
+    const all = [];
+
+    return (issue, path) => {
+      if (!issue) {
+        return;
+      }
+      if (issue === 'complete') {
+        console.log(JSON.stringify(all));
+        if (!opts.nostatus) {
+          process.exit(all.length === 0 ? 0 : 1);
+        }
+      }
+      issue.path = path;
+      all.push(issue);
+    };
+  },
+};
+
+function pluralize(str, n) {
+  return n === 1 ? str : `${str}s`;
 }
 function commentSafeRe(word) {
-  var options = [
-    '\\b' + word.toUpperCase() + '\\b',
-    '@' + word + '\\b'
-  ].join("|") 
+  const options = [
+    `\\b${word.toUpperCase()}\\b`,
+    `@${word}\\b`,
+  ].join('|');
+
   return new RegExp(options);
 }
 
@@ -140,3 +172,5 @@ function commentSafeRe(word) {
 function stripShebang(src) {
   return src.replace(/^#![^\n]*/, '');
 }
+
+module.exports = findTodos;
